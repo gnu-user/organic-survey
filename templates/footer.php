cbd913e52fe86efece8e2a52c81e98f81795dde2
gnu_user <gsoc.student@gmail.com>
2013-02-09 01:24:38 -0500

    Added the header and footer templates.

---
+++ templates/footer.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ *  Organic Food Survey
+ *
+ *  Copyright (C) 2013 Jonathan Gillett
+ *  All rights reserved.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+?>
+      <footer class="footer">
+        <div class="pull-left">
+          <p>Designed and built by <a target="_blank" href="https://github.com/gnu-user/organic-survey">Jonathan Gillett</a></p>
+          <p>This website is Open Source, code licensed under the 
+            <a target="_blank" href="http://www.gnu.org/licenses/agpl-3.0.html">GNU Affero General Public License, Version 3</a>
+          </p>
+          <p><strong>&copy; Jonathan Gillett</strong></p>
+        </div>
+      </footer>
+    </div>
+    <!-- Javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery-1.9.0.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ *  Organic Food Survey
+ *
+ *  Copyright (C) 2013 Jonathan Gillett
+ *  All rights reserved.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <!-- The title of the page -->
+    <title>Organic Food Survey</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Organic Food Survey">
+    <meta name="author" content="Jonathan Gillett">
+
+    <!-- CSS styles -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+    <link href="css/bootswatch.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.min.js"></script>
+    <![endif]-->
+  
+  </head>
+  <body>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <a class="brand" href="index.php">
+            Organic Food Survey
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="container">
