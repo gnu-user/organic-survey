b018f09bf973905c2b7034d808562c6d9c2244ef
gnu_user <gsoc.student@gmail.com>
2013-02-09 02:24:19 -0500

    Added extra-width for the horizontal forms

--- templates/header.php
+++ templates/header.php
@@ -30,7 +30,7 @@
     <meta name="author" content="Jonathan Gillett">
 
     <!-- CSS styles -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
     <link href="css/bootswatch.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet">
