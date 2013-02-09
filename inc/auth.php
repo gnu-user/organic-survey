35850af35fd2ecfffcdb43b457f589ada0e57d4b
gnu_user <gsoc.student@gmail.com>
2013-02-09 13:01:12 -0500

    Minor comment refactor.

--- inc/auth.php
+++ inc/auth.php
@@ -22,8 +22,7 @@
 
 /**
  * The following is a configuration file containing all of the authentication 
- * information such as the database access and database names as well as the 
- * AES encrypt/decrypt keys.
+ * information such as the database access and database names.
  * 
  * Basically, anything you wouldn't want everyone else to view and have access
  * to goes in this file, as it is not part of the open source code that is
