8efd0e12ffa9621354d234758b5b435a3b896bde
gnu_user <gsoc.student@gmail.com>
2013-02-09 02:26:27 -0500

    Added the organic food survey

---
+++ templates/survey.php
@@ -0,0 +1,132 @@
+<?php
+/*
+ *  Organic Food Survey
+ *
+ *  Copyright (C) 2013 Jonathan Gillett
+ *  All rights reserved.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+?>
+<section id="register">
+    <div class="page-header">
+        <h1>Survey</h1>
+    </div>
+    <div class="row">
+        <div class="span8">
+            <!-- Info about the survey -->
+            <div id="activeinfo" class="alert alert-info">
+                <button type="button" class="close" data-dismiss="alert">×</button>
+                <p>
+                    Fill out the following survey to the best of your abilities, if there are any questions
+                    for which you need clarification <strong>please let us know</strong>.
+                </p>
+            </div>
+            <form class="well form-horizontal" action="active.php" method="post" accept-charset="UTF-8">
+                <fieldset>
+                    <!-- TASTE -->
+                    <div class="control-group">
+                        <label for="taste" class="control-label"><strong><strong>The pizza taste was:</strong></strong></label>
+                        <div class="controls">
+                            <select id="taste" name="taste" class="input-xlarge">
+                                <option></option>
+                                <option value="1">Worst tasting pizza I've ever had</option>
+                                <option value="2">Barely edible</option>
+                                <option value="3">Mediocre</option>
+                                <option value="4">Good</option>
+                                <option value="5">Best tasting pizza I've ever had</option>
+                            </select>
+                        </div>
+                    </div>
+                    <!-- TEXTURE -->
+                    <div class="control-group">
+                        <label for="texture" class="control-label"><strong>The texture was:</strong></label>
+                        <div class="controls">
+                            <select id="texture" name="texture" class="input-xlarge">
+                                <option></option>
+                                <option value="1">Worst texture I've ever had</option>
+                                <option value="2">Barely edible</option>
+                                <option value="3">Mediocre</option>
+                                <option value="4">Good</option>
+                                <option value="5">Best texture I've ever had</option>
+                            </select>
+                        </div>
+                    </div>
+                    <!-- CRUST -->
+                    <div class="control-group">
+                        <label for="crust" class="control-label"><strong>The taste of the crust was:</strong></label>
+                        <div class="controls">
+                            <select id="crust" name="crust" class="input-xlarge">
+                                <option></option>
+                                <option value="1">Worst crust I've ever tasted</option>
+                                <option value="2">Barely edible</option>
+                                <option value="3">Mediocre</option>
+                                <option value="4">Good</option>
+                                <option value="5">Best crust I've ever tasted</option>
+                            </select>
+                        </div>
+                    </div>
+                    <!-- TOPPINGS -->
+                    <div class="control-group">
+                        <label for="toppings" class="control-label"><strong>The taste of the toppings was:</strong></label>
+                        <div class="controls">
+                            <select id="toppings" name="toppings" class="input-xlarge">
+                                <option></option>
+                                <option value="1">Worst tasting toppings I've ever had</option>
+                                <option value="2">Barely edible</option>
+                                <option value="3">Mediocre</option>
+                                <option value="4">Good</option>
+                                <option value="5">Best tasting toppings I've ever had</option>
+                            </select>
+                        </div>
+                    </div>
+                    <!-- VISUAL -->
+                    <div class="control-group">
+                        <label for="taste" class="control-label"><strong>The visual appeal was:</strong></label>
+                        <div class="controls">
+                            <select id="taste" name="taste" class="input-xlarge">
+                                <option></option>
+                                <option value="1">Worst looking pizza I've ever seen</option>
+                                <option value="2">Poor aesthetic appeal</option>
+                                <option value="3">Mediocre aesthetic appeal</option>
+                                <option value="4">Good aesthetic appeal</option>
+                                <option value="5">Best looking pizza I've ever seen</option>
+                            </select>
+                        </div>
+                    </div>
+                    <!-- AROMA -->
+                    <div class="control-group">
+                        <label for="taste" class="control-label"><strong>The aroma was:</strong></label>
+                        <div class="controls">
+                            <select id="taste" name="taste" class="input-xlarge">
+                                <option></option>
+                                <option value="1">Worst smelling pizza I've ever had</option>
+                                <option value="2">Poor aroma</option>
+                                <option value="3">Mediocre aroma</option>
+                                <option value="4">Good aroma</option>
+                                <option value="5">Best smelling pizza I've ever had</option>
+                            </select>
+                        </div>
+                    </div>
+                    <!-- Submit survey -->
+                    <div class="control-group">
+                        <div class="controls">
+                            <button type="submit" id="survey" name="survey" class="btn btn-large btn-inverse">Submit</button>
+                        </div>
+                    </div>
+                </fieldset>
+            </form>
+        </div>
+    </div>
+</section>
